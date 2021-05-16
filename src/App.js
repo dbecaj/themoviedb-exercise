b55239c19d5f612c0ed9455e61291d17118bed15
dbecaj <dominik.becaj@gmail.com>
2021-05-16 19:00:39 +0200

    Added example .env and now fetching data from themoviedb-api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { useEffect, useState } from "react";
 import Genres from "./components/Genres";
 import Layout from "./components/Layout";
 import Movie from "./components/Movie";
 import Pagination from "./components/Pagination";
 import SideBar from "./components/SideBar";
 import UserScore from "./components/UserScore";
+
+const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY
 
 const genres = [
   'Action',
@@ -28,6 +31,26 @@ const genres = [
 ]
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    if (movies.length === 0) {
+      fetch('https://api.themoviedb.org/3/movie/now_playing', {
+        method: 'GET',
+        headers: new Headers({
+          'Authorization': `Bearer ${TMDB_API_KEY}`,
+          'Content-Type': 'application/json;charset=utf-8'
+        })
+      }).then(response => response.json())
+        .then(data => {
+          console.log("got data!")
+          setMovies(data)
+        })
+    }
+  })
+
+  console.log("render")
+
   return (
     <Layout>
       <div class="flex">
@@ -38,7 +61,7 @@ function App() {
           <Genres genres={genres} allSelected={[]} />
 
           <h3 class="text-lg font-bold my-4">User score</h3>
-          <UserScore score={0}/>
+          <UserScore score={0} />
 
           <button type="button" class="w-full mt-8 p-2 bg-blue-500 text-white font-bold text-xl rounded-lg">Search</button>
         </SideBar>
@@ -46,14 +69,15 @@ function App() {
         { /* Movies */}
         <div class="w-full">
           <div class="grid gap-x-8 gap-y-12 justify-around" style={{ gridTemplateColumns: "repeat(auto-fit, minmax(220px, auto))" }}>
-            {Array.from(Array(20)).map(v => {
+            {movies.results?.map(movie => {
+              console.log(movie)
               return (
                 <Movie
-                  imgSrc="https://www.themoviedb.org/t/p/w220_and_h330_face/6vcDalR50RWa309vBH1NLmG2rjQ.jpg"
-                  imgAlt="The Marksman"
-                  title="The Marksman"
-                  userScore="74"
-                  releaseDate="15.05.2021"
+                  imgSrc={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`}
+                  imgAlt={movie.title}
+                  title={movie.title}
+                  userScore={Math.trunc(movie.vote_average * 10)}
+                  releaseDate={movie.release_date}
                 />
               )
             })}
